4eeb9cbc5845e42aa77f5e440fd883d42f21699e
mattharter <matt.harter@genesys.com>
2020-10-26 20:25:58 -0400

    use new geojson, add zoom and pan, and color the oceans

--- javascript/world_map.js
+++ javascript/world_map.js
@@ -1,21 +1,60 @@
-const worldMapHeight = 200;
-const worldMapWidth = 200;
+const worldMapHeight = 600;
+const worldMapWidth = 600;
 
 // A lot of the map drawing was pull from https://medium.com/swlh/data-visualization-with-d3-world-map-aa03d68eb906
 const createWorldMap = (mapData) => {
+    console.log(mapData);
     const projection = d3version6.geoMercator()
         .fitSize([worldMapWidth, worldMapHeight], mapData)
         .precision(100);
     const pathGenerator = d3version6.geoPath().projection(projection);
 
-    d3version6.select('#world-map')
+    function handleZoom(event) {
+        d3version6.select('#world-map g').attr("transform", `${event.transform}`);
+    }
+
+    let zoom = d3version6.zoom()
+        .scaleExtent([1, 10])
+        .on("zoom", handleZoom)
+
+
+    // This is my hacky way around the oceans not being paths.  Since they aren't defined (just empty space) zoom/pan won't
+    // work when your mouse is pointed on the oceans.  This bounding box fixes that.
+    let data = [[0, 0], [0, worldMapHeight-12], [worldMapWidth, worldMapHeight-12], [worldMapWidth, 0]];
+    let lineGenerator = d3version6.line();
+    let pathString = lineGenerator(data);
+
+    let map = d3version6.select('#world-map')
+        .append('g')
+        .call(zoom)
+    map
+        .append("path")
+        .attr('d', pathString)
+        .attr('fill', 'lightblue')
+        .style('pointer-events', 'all')
+    map
+        // .call(zoom)
         .selectAll('.country')
         .data(mapData.features)
+        // .data(mapData.features)
         .join('path')
         .attr('class', 'country')
-        .attr('d', feature => pathGenerator(feature))
+        .attr('fill', 'white')
+        .style('pointer-events', 'all')
+        .style("stroke-width", ".3")
+        .style('stroke', 'black')
+        .attr('d', feature => {
+            console.log(feature);
+            return pathGenerator(feature);
+        })
+        // .attr('d',  d3version6.geoPath())
         .style('width', `${worldMapWidth}px`)
         .style('height', `${worldMapHeight}px`)
+        .style('border', 'solid black')
+
+
+
+
 }
 
 worldMapPromise.then(data => {
