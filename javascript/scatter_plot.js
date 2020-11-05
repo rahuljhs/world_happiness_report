d4252fca54cf47bbd51769bb5c88cc4c63447939
Paige Scott <paigescott@LSNLY-MBP064.local>
2020-11-04 21:06:24 -0500

    scatter plot and filter updates

--- javascript/scatter_plot.js
+++ javascript/scatter_plot.js
@@ -8,12 +8,16 @@ var plotwt = scatterPlotWidth - margin.left - margin.right;
 
 const splot = d3version6.select('#scatter-plot');
 
-var year = '2015'; //this will need to be updated by the filter input: Year
-var attr1 = 'Happiness Score'; //this will need to be updated by the filter input: Attribute 1
-var attr2 = 'Freedom'; //this will need to be updated by the filter input: Attribute 2
 
 // The color is just there to make sure code is working.  Feel Free to delete when working in this file!
 const createScatterPlot = () => {
+    var year = document.getElementById("year").value; //this will need to be updated by the filter input: Year
+    console.log(year);
+    var attr1 = document.getElementById("attribute1").value; //this will need to be updated by the filter input: Attribute 1
+    console.log(attr1);
+    var attr2 = document.getElementById("attribute2").value; //this will need to be updated by the filter input: Attribute 2
+    console.log(attr2);
+
     d3version6.select('#scatter-plot')
         .style('width', `${scatterPlotWidth}px`)
         .style('height', `${scatterPlotHeight}px`)
@@ -39,9 +43,9 @@ const createScatterPlot = () => {
         .style('stroke-width', '1');
 
     //get min & max of x & y values
-    var xmin = 10; //attr 1
+    var xmin = 100; //attr 1
     var xmax = 0; //attr 1
-    var ymin = 10; //attr 2
+    var ymin = 100; //attr 2
     var ymax = 0; //attr 2
     for (x = 0; x < dataHash[year].length; x++) {
         if (dataHash[year][x][attr1] > xmax) {
@@ -59,7 +63,7 @@ const createScatterPlot = () => {
     }
 
     //define div for tooltip: example code used: https://bl.ocks.org/d3noob/a22c42db65eb00d4e369
-    var ttdiv = splot.append("div")
+    var div = d3version6.select("body").append("div")
         .attr("class", "tooltip")
         .style("opacity", 0);
 
@@ -74,7 +78,22 @@ const createScatterPlot = () => {
             .attr('cx', (dataHash[year][x][attr1]) * xscale + margin.left)
             .attr('cy', plotht + margin.top - (dataHash[year][x][attr2] * yscale))
             .attr('r', '3')
-            .attr('fill', 'black');
+            .attr('fill', 'black')
+            .attr('id', dataHash[year][x]['Country'])
+            .on("mouseover", function (event, d) {
+                console.log("mouseover");
+                div.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                div.html("country")
+                    .style("left", "30px")
+                    .style("top", "20px");
+            })
+            .on("mouseout", function (d) {
+                div.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            });
         //should add tooltip on this
     }
 
@@ -82,20 +101,20 @@ const createScatterPlot = () => {
     //x-axis label
     splot.append('text')
         .text(attr1)
-        .style('text-anchor', 'center')
+        .style('text-anchor', 'middle')
         .attr('x', scatterPlotWidth / 2)
         .attr('y', scatterPlotHeight - 5);
     //y-axis label
     splot.append('text')
         .text(attr2)
-        .style('text-anchor', 'center')
+        .style('text-anchor', 'middle')
         .attr('transform', 'translate(' + (margin.left / 3) + ',' + ((plotht) / 2 + margin.top) + ') rotate(-90)');
     //scatter plot title
     splot.append('text')
-        .text(attr2 + " vs. " + attr1)
+        .text(attr2 + " vs. " + attr1 + " in " + year)
         .attr('x', margin.left + plotwt/2)
         .attr('y', 30)
-        .style('font-size', "20px")
+        .style('font-size', "16px")
         .style('text-anchor', 'middle');
 
 
@@ -110,7 +129,7 @@ const createScatterPlot = () => {
         .ticks(10);
     splot.append('g')
         .attr('class', 'axis')
-        .attr('transform', 'translate(0, ' + (margin.top + plotht)+ ')')
+        .attr('transform', 'translate(0, ' + (margin.top + plotht) + ')')
         .call(xAxis);
     //y-axis
     var yAxisScale = d3version3.scale.linear()
@@ -125,6 +144,7 @@ const createScatterPlot = () => {
         .attr('transform', 'translate(' + (margin.left) + ",0)")
         .call(yAxis);
 
+    //console.log("Axis: " + d3version3.select(".axis path").style("stroke-width"))
 
 }
 
@@ -132,3 +152,24 @@ allFilesPromise.then(() => {
     createScatterPlot();
 });
 
+document.getElementById("year").onchange = function () {
+    //console.log("year update");
+    d3version3.selectAll("#scatter-plot > *").remove(); 
+    allFilesPromise.then(() => {
+        createScatterPlot();
+    });
+}
+document.getElementById("attribute1").onchange = function () {
+    //console.log("attr1 update");
+    d3version3.selectAll("#scatter-plot > *").remove(); 
+    allFilesPromise.then(() => {
+        createScatterPlot();
+    });
+}
+document.getElementById("attribute2").onchange = function () {
+    //console.log("attr2 update");
+    d3version3.selectAll("#scatter-plot > *").remove(); 
+    allFilesPromise.then(() => {
+        createScatterPlot();
+    });
+}
